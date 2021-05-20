5bfe083adfe8478e70cf4e074e9d1a19fdeccfd5
juanma1980 <juanma1980@gmail.com>
2021-05-20 08:13:33 +0200

    clean code

--- zero-lliurex-mirror-redirect.install/usr/share/mirror-redirect/stacks/main.py
+++ zero-lliurex-mirror-redirect.install/usr/share/mirror-redirect/stacks/main.py
@@ -1,15 +1,9 @@
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QGridLayout,QLineEdit,QHBoxLayout,QComboBox,QCheckBox
+from PySide2.QtWidgets import QApplication,QWidget,QGridLayout,QCheckBox
 from PySide2 import QtGui
 from PySide2.QtCore import Qt
 from appconfig.appConfigStack import appConfigStack as confStack
 from appconfig.appConfigN4d import appConfigN4d
-import lliurex.interfacesparser
 import os
-import subprocess
-import time
-import threading
-import sys
-import ssl
 import yaml
 
 import gettext
@@ -30,11 +24,8 @@ class main(confStack):
 	def _load_screen(self):
 		self._set_server_data()
 		box=QGridLayout()
-		#self.statusBar=QAnimatedStatusBar.QAnimatedStatusBar()
-		#box.addWidget(self.statusBar)
 		self.setLayout(box)
 		self.chkEnabled=QCheckBox(_("Enable mirror redirection"))
-		#self.chkEnabled.stateChanged.connect(self._on_sw_state)
 		box.addWidget(self.chkEnabled,0,0,1,1)
 		self.updateScreen()
 		return(self)
@@ -42,22 +33,15 @@ class main(confStack):
 
 	def updateScreen(self):
 		self._set_server_data()
-			#self.name.setDescription(_("name of the repository"),_("Insert repository name"))
 		if self.is_enabled():
 			self.chkEnabled.setChecked(True)
 	#def _udpate_screen
-
-	def _on_sw_state(self,state):
-			pass
-	#def _on_sw_state
 
 	def is_enabled(self):
 		sw_enabled=False
 		self.slave_ip=self._get_replication_ip()
 		self._debug("Slave IP: {}".format(self.slave_ip))
 		try:
-				#sw_enabled=self.n4dMaster.is_mirror_shared("","NfsManager","/net/mirror/llx19",self.slave_ip)['status']
-			#resp=self.n4d_master.n4dQuery("NfsManager","is_mirror_shared",self.mirror_dir,self.slave_ip)
 			resp=self.n4dQuery("NfsManager","is_mirror_shared",self.mirror_dir,self.slave_ip,ip=self.master_ip)
 			if isinstance(resp,dict):
 				if resp.get('status',-1)==0:
@@ -66,7 +50,6 @@ class main(confStack):
 				if resp=="Shared is configured":
 					sw_enabled=True
 		except Exception as e:
-				#self.showMsg("Error Code: {}".format(resp.get('status',-1)))
 			self._debug(e)
 			sw_enabled=False
 		self._debug("Redirect enabled: {}".format(sw_enabled))
@@ -74,7 +57,6 @@ class main(confStack):
 	#def is_enabled
 	
 	def _get_replication_ip(self):
-				
 		path="/etc/netplan/30-replication-lliurex.yaml"
 		try:
 			if os.path.exists(path):
@@ -86,11 +68,9 @@ class main(confStack):
 			print("Failed getting replication IP")
 			print(e)
 			raise e		
-		
 	#def _get_replication_ip
 	
 	def _set_server_data(self):
-			#self.master_ip=self.n4dQuery("VariablesManager","get_variable","MASTER_SERVER_IP")
 		master_ip=self.n4dGetVar(var="MASTER_SERVER_IP")
 		self._debug("Get master_ip: {}".format(master_ip))
 		self.master_ip=''
@@ -108,18 +88,8 @@ class main(confStack):
 	
 	def enable_redirect(self):
 		sw_add=False
-		#if not os.path.isdir(self.mirror_dir):
-		#	try:
-		#		os.makedirs(self.mirror_dir)
-		#	except:
-		#		self._debug("Can't create dir %s"%self.mirror_dir)
-		#		self.showMsg(_("Can't create dir {}".format(self.mirror_dir)))
-		#		return sw_add
-		
 		try:
-				#sw_add=self.n4d_master.n4dQuery("NfsManager","add_mirror",self.mirror_dir,self.slave_ip)
 			mount_stat=self.n4dQuery("NfsManager","is_mount_configured","{}".format(self.mirror_dir))
-			sw_add=True
 			if isinstance(mount_stat,dict):
 				if mount_stat.get('status',0)==-1:
 					self._debug("Mounting on boot")
@@ -127,7 +97,7 @@ class main(confStack):
 					sw_add=True
 		except Exception as e:
 			print("Add mirror err: {}".format(e))
-			self.showMsg(_("Add mirror error {}".format(e)))
+			self.showMsg(_("Error adding mirror {}".format(e)))
 			sw_add=False
 		self.n4dQuery("NfsManager","add_mirror",self.mirror_dir,self.slave_ip,ip=self.master_ip)
 		return sw_add
@@ -137,7 +107,6 @@ class main(confStack):
 		sw_rm=True
 		try:
 			self.n4dQuery("NfsManager","remove_mount_on_boot",self.mirror_dir)
-			#self.n4d_master.n4dQuery("NfsManager","remove_ip_from_mirror",self.mirror_dir,self.slave_ip)
 		except:
 			sw_rm=False
 		self.n4dQuery("NfsManager","remove_ip_from_mirror",self.mirror_dir,self.slave_ip,ip=self.master_ip)
@@ -147,21 +116,14 @@ class main(confStack):
 	def writeConfig(self):
 		state=self.chkEnabled.isChecked()
 		self._debug("State changed to {}".format(state))
-#widget.set_sensitive(False)
-#		self.spinner.start()
 		if state:
 			self._debug("Redirecting mirror...")
-#			self.lbl_state.set_text(_("Redirecting mirror..."))
-			#th=threading.Thread(target=self.enable_redirect)
-			#th.start()
 			if not self.enable_redirect():
 				self.chkEnabled.setChecked(False)
 		else:
 			self._debug(_("Redirecting mirror..."))
 			if not self.disable_redirect():
 				self.chkEnabled.setChecked(False)
-#			th=threading.Thread(target=self.redirectMirror.disable_redirect)
-#			th.start()
 		self._debug("Done")
 	#def writeConfig
 
